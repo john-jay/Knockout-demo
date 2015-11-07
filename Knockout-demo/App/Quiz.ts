59ede6018a6fc3ebf02f49edede6ab9da9ffa611
John Holliday <johnmholliday@gmail.com>
2015-11-07 10:16:34 -0600

    Add jquery. Read json and display all items.

--- Knockout-demo/App/Quiz.ts
+++ Knockout-demo/App/Quiz.ts
@@ -1,16 +1,21 @@
-﻿interface ITestItem {
-    def: string;
-    word: string;
-}
+﻿
 
 class Quiz {
-    private testItems: ITestItem[];
-    public curerntItem = 1;
-    public item: ITestItem;
+    public testItems = ko.observableArray<VocItem>();
+    //public curerntItem = 1;
+    //public item: VocItem;
 
     constructor() {
-        this.item = { def: "gee whiz", word: "gosh" };
-        //this.testItems.push({ def: "gee whiz", word: "gosh" });
+        this.loadData();
+    }
+
+    loadData() {
+        $.getJSON("/Assets/VocTest.json", data => {
+            var items = $.map(data.lesson, item => {
+                return new VocItem(item);
+            });
+            this.testItems(items);
+        });
     }
 }
 
