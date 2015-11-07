bbc0561c62ae43ae4484be75964bd3a5ca64cff5
John Holliday <johnmholliday@gmail.com>
2015-11-07 09:17:28 -0600

    Add view model, ko typings, style.

--- Knockout-demo/App/Quiz.ts
+++ Knockout-demo/App/Quiz.ts
@@ -1,1 +1,17 @@
-﻿
+﻿interface ITestItem {
+    def: string;
+    word: string;
+}
+
+class Quiz {
+    private testItems: ITestItem[];
+    public curerntItem = 1;
+    public item: ITestItem;
+
+    constructor() {
+        this.item = { def: "gee whiz", word: "gosh" };
+        //this.testItems.push({ def: "gee whiz", word: "gosh" });
+    }
+}
+
+ko.applyBindings(new Quiz());
